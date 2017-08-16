e403ea1e18d99953910ca8eca82aee765a1ba6ec
BraeWebb <email@braewebb.com>
2017-08-16 06:11:52 +0000

    Documented Game
    Documented the game.c file.

--- game.c
+++ game.c
@@ -76,26 +76,50 @@ int* prompt(Game* game) {
     }
 }
 
+
+/**
+ * Uses the algorithm described below to determine the position of an
+ * automatic players next move based on the state of the game.
+ *
+ * Returns an array of length two containing the x and y positions respectively
+ *
+ * Algorithm:
+ *      let n be number of moves of current automatic player
+ *      let t be an intermediate value
+ *      let height be the amount of rows in the grid
+ *      let width be the amount of columns in the grid
+ *      let m be the greatest value of height and width
+ *      then
+ *          if player is player1
+ *              t = (n * 9 % 1000037) + 17
+ *          otherwise
+ *              t = (n * 7 % 1000213) + 81
+ *
+ *          x = (t / m) mod height
+ *          y = t mod width
+ *      if the position is taken
+ *          increment number of moves and retry
+ */
 int* automatic_move(Game* game) {
     int gridHeight = game->grid->rows;
     int gridWidth = game->grid->columns;
 
-    int m = gridHeight > gridWidth ? gridHeight : gridWidth;
+    int maxDimension = gridHeight > gridWidth ? gridHeight : gridWidth;
 
-    int n, t, row, column;    
+    int moves, intermediate, row, column;
 
     while (1) {
         
-        n = game->moves[game->turn];
+        moves = game->moves[game->turn];
 
         if (game->turn == 0) {
-            t = ((n * 9 % 1000037) + 17);
+            intermediate = ((moves * 9 % 1000037) + 17);
         } else {
-            t = ((n * 7 % 1000213) + 81);
+            intermediate = ((moves * 7 % 1000213) + 81);
         }
     
-        row = (t / m) % gridHeight;
-        column = t % gridWidth;
+        row = (intermediate / maxDimension) % gridHeight;
+        column = intermediate % gridWidth;
 
         if (game->grid->get(game->grid, row, column) != 0) {
             game->moves[game->turn]++;
@@ -111,13 +135,29 @@ int* automatic_move(Game* game) {
     return result;
 }
 
+/**
+ * Determines the winner, if any, of the game based on the games state.
+ *
+ * Returns 
+ *      0 iff no winner.
+ *      1 iff player 1 wins.
+ *      2 iff player2 wins.
+ * 
+ */
 int get_winner(Game* game) {
+    // The maximum possible amount of positions in a trail through the grid.
     int maxSize = game->grid->rows * game->grid->columns;
     
+    // Determine if player 1 is a winner.
+    // Looks from the first position in each row and checks if a path
+    // can be made to a position in the last column.
     for (int x = 0; x < game->grid->rows; x++) {
+        // Only check player 1 tiles.
         if (game->grid->get(game->grid, x, 0) == 1) {
+            // Find all tiles connected to the origin.
             int** matches = game->grid->search(game->grid, x, 0);
             
+            // Return 1 if any of the connected tiles are in the last column.
             for (int i = 0; i < maxSize; i++) {
                 if (matches[i] != NULL) {
                     if (matches[i][1] == game->grid->columns - 1) {
@@ -128,10 +168,16 @@ int get_winner(Game* game) {
         }
     }
 
+    // Determine if player 2 is a winner.
+    // Looks from the first position in each column and checks if a path can be
+    // made to a position in the last row.
     for (int y = 0; y < game->grid->columns; y++) {
+        // Only check player 2 tiles.
         if (game->grid->get(game->grid, 0, y) == 2) {
+            // Find all tiles connected to the origin.
             int** matches = game->grid->search(game->grid, 0, y);
-
+            
+            // Return 1 if any of the connected tiles are in the last row.
             for (int i = 0; i < maxSize; i++) {
                 if (matches[i] != NULL) {
                     if (matches[i][0] == game->grid->rows - 1) {
@@ -145,7 +191,15 @@ int get_winner(Game* game) {
     return 0;
 }
 
+/**
+ * Checks to see if the game is over by checking if there is a winner or
+ * if the grid is full.
+ *
+ * Returns 1 iff the game is over, otherwise 0.
+ *
+ */
 int is_game_over(Game* game) {
+    // Determine if the game has a winner
     if (get_winner(game) != 0) {
         return 1;
     }
@@ -161,34 +215,55 @@ int is_game_over(Game* game) {
     return 1;
 }
 
+/**
+ * Makes the next move based on the current state of the game.
+ *
+ * Either prompts user for input if the next turn is a manual player or
+ * generates the next move of the automatic player.
+ * Then takes those positions and makes that move.
+ *
+ */
 void take_turn(Game* game) {
     int* move = malloc(sizeof(int) * 2);
 
     if (game->modes[game->turn] == 0) {
+        // If it's an automatic players turn then generate the move.
         move = automatic_move(game);
         printf("Player %c => %d %d\n", game->turn ? 'X' : 'O', move[0], move[1]);
     } else {
+        // If it's a manual players turn then prompt for their move.
         move = prompt(game);
     }
+    // Update the game based on the selected move.
     game->grid->set(game->grid, move[0], move[1], (game->turn + 1));
     game->moves[game->turn]++;
     game->turn = game->turn ? 0 : 1;
 }
 
+
+/**
+ * Creates a new Game struct and sets the values of it's properties
+ *
+ * Returns a pointer to the new Game struct.
+ *
+ */
 Game* create_game(Grid* grid, int player1Mode, int player2Mode) {
     Game* game = malloc(sizeof(Game));
     
+    // Allocates space for the player modes and the amount of moves made.
     int* modes = malloc(sizeof(int) * 2);
     int* moves = calloc(2, sizeof(int));
 
     modes[0] = player1Mode;
     modes[1] = player2Mode;
     
+    // Initializes struct properties
     game->turn = 0;
     game->modes = modes;
     game->moves = moves;
     game->grid = grid;
 
+    // Assigns local functions to struct properties for a 'class' experience.
     game->move = take_turn;
     game->is_over = is_game_over;
     game->winner = get_winner;
